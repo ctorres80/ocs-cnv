2ae64cf18e8c3c5db6b202e62d1ef90038c193aa
ctorres80 <ctorres@redhat.com>
2020-05-29 09:25:34 +0200

    ok

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,2 +1,12 @@
+# requirements for
 sudo yum install -y python2-openshift.noarch
 sudo yum install -y python2-openshift.noarch
+
+# get worker nodes
+oc get nodes -l node-role.kubernetes.io/worker= -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+# discovery CPUs in workers
+oc get nodes -l node-role.kubernetes.io/worker= -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | while read node; do oc debug node/ip-10-0-147-218.eu-central-1.compute.internal -- lscpu|grep "^CPU(s)"; done
+# discovery RAM
+oc get nodes -l node-role.kubernetes.io/worker= -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | while read node; do oc debug node/ip-10-0-147-218.eu-central-1.compute.internal -- free -h; done
+# get the OCS workers
+oc get nodes -l cluster.ocs.openshift.io/openshift-storage= -o jsonpath='{range .items[*]}{@.metadata.name}{"\n"}'
