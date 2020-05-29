16babdc7160735f852136cb15d966faf47219f0e
ctorres80 <ctorres@redhat.com>
2020-05-29 09:43:23 +0200

    ok

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -10,3 +10,7 @@ oc get nodes -l node-role.kubernetes.io/worker= -o jsonpath='{range .items[*]}{.
 oc get nodes -l node-role.kubernetes.io/worker= -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | while read node; do oc debug node/$node -- free -h; done
 # get the OCS workers
 oc get nodes -l cluster.ocs.openshift.io/openshift-storage= -o jsonpath='{range .items[*]}{@.metadata.name}{"\n"}'
+# label namespace openshift-storage for monitoring
+oc label namespace openshift-storage "openshift.io/cluster-monitoring=true"
+# enable ceph-tool in ocs
+oc patch OCSInitialization ocsinit -n openshift-storage --type json --patch  '[{ "op": "replace", "path": "/spec/enableCephTools", "value": true }]'
